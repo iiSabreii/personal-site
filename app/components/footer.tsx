31ca7bd22ba3ed06697d618670ca29e5ea68a03d
sabre <carowell04@gmail.com>
2026-04-24 23:30:19 -0500

    wip

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/footer.module.css';
 import Image from 'next/image';
 
 import solarSystem from "../images/solar-system.png"
+import rocket from "../images/rocket.png";
 
 interface FooterItemLink {
     label: string;
@@ -16,14 +17,17 @@ const links: FooterItemLink[] = [
 
 export default function Footer() {
     return (
-        <footer>
-            <nav className={styles.nav}>
-                <span className={styles.solarEasterEgg}>
-                    <a href="#">
-                        <Image src={solarSystem} alt="Just a solar system, Nothing to see here" />
-                    </a>
-                </span>
-            </nav>
+        <footer className={styles.footer}>
+            <div className={styles.solarEasterEgg}>
+                <a href="#">
+                    <Image src={solarSystem} alt="Just a solar system, Nothing to see here" />
+                </a>
+            </div>
+            <div className={styles.rocket}>
+                <a href="#top">
+                    <Image src={rocket} alt="Rocket Ship" />
+                </a>
+            </div>
         </footer>
     )
 }
